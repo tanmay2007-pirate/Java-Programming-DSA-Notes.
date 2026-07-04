b465b4f22c144245740c4dad9872dbd4b145ef30
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-07-04 18:09:08 +0530

    adding recursion part 2

--- Recursion/recursion.java
+++ Recursion/recursion.java
@@ -373,5 +373,212 @@
 // p11 :- Tiling problem .
 
 
+// given  a "2 x n" floor and tiles of size "2 x 1 " , count the number of ways to tile the given board using the 2X1 tiles. (A tile can either be placed horizantally of vertically.)
+
+
+
+
+
+// public class recursion{
+
+//     public static int tilingproblem(int n){         // 2xn is board length
+
+//         if(n == 0 || n == 1){
+//             return 1;
+//         }
+
+//         //kaam
+//         // vertical choice
+
+//         int fnm1 = tilingproblem(n-1);
+
+//         // horizantal choice
+
+//         int fnm2 = tilingproblem(n-2);
+
+
+//         //Total ways.
+//         int totways = fnm1 + fnm2;
+
+//         return totways;
+
+//     } 
+//     public static void main(String args[]){
+
+
+//         System.out.println(tilingproblem(4));
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// remove duplicates from the string
+
+
+
+// public class recursion {
+
+//     public static void remove_duplicates(String str , int idx ,StringBuilder newstring , boolean map[]){
+
+//         if(idx == str.length()){
+//             System.out.println(newstring);
+//             return ;
+//         }
+
+//         // kaam 
+
+//         char currchar = str.charAt(idx);
+
+//         if(map[currchar - 'a'] == true){
+//             //duplicates
+//             remove_duplicates(str , idx+1 , newstring ,map);
+//         }else{
+//             map[currchar - 'a'] = true;
+//             remove_duplicates(str , idx+1 , newstring.append(currchar) , map);
+//         }
+
+//     }
+//     public static void main(String args[]){
+
+
+//         String str = "appnnacollage";
+
+//         remove_duplicates(str , 0 , new StringBuilder("") , new boolean[26]);
+
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// friends pairing problem
+
+
+// given n friends , each one can remain single or can be paired up with some other friend . each can be paired only once . find out the total number of ways in which friends can  remain single or can be paired up.
+
+
+// public class recursion{
+
+//     public static int friends_pairing(int n){
+
+//         // base
+
+//         if(n== 1 || n == 0){
+//             return 1;
+//         }
+//         // choice
+//         // single
+
+//         int fnm1 = friends_pairing(n-1);
+
+
+
+//         // pair
+
+//         int fnm2 = friends_pairing(n-2);
+//         int pairways = (n-1) * fnm2;
+
+//         // totways 
+//         int totways = fnm1 + pairways;
+//         return totways;
+
+
+//         //return friends_pairing(n-1) + (n-1)*friends_pairing(n-2);
+
+//     }
+//     public static void main(String args[]){
+
+//         System.out.println(friends_pairing(3));
+
+
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Q). Binary Strings Problem.
+
+// print all binary strings of size N without consecutive ones.
+
+
+
+// public class recursion{
+
+//     public static void printBinStrings(int n , int lastplace , String str){
+
+//         // base case
+
+//         if( n == 0){
+//             System.out.println(str);
+//             return;
+//         }
+
+
+//         //kaam
+
+//         printBinStrings(n-1 , 0 , str + "0");
+
+//         if(lastplace == 0){
+//             printBinStrings(n-1 , 1 , str + "1");
+//         }
+
+
+//     }
+    
+//     public static void main(String args[]){
+
+
+//         printBinStrings(3, 0 ,"");
+
+
+//     }
+// }
+
 
 
