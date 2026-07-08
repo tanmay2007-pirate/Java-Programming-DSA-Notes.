84ea94cad02673399038a6513a84a6e361c66b0f
Tanmay Patharkar <tanmaypatharkar2007@gmail.com>
2026-07-08 23:02:34 +0530

    merge sort

--- Sorting Algorithms/Sorting_algo.java
+++ Sorting Algorithms/Sorting_algo.java
@@ -263,4 +263,98 @@
 
 
 
+// DIVIDE AND CONQUOR.
+
+//  MERGE SORT.
+
+
+
+public class Sorting_algo{
+
+    public static void print_arr(int arr []){
+        for(int i = 0 ; i <  arr.length ; i++){
+            System.out.print(arr[i] +" ");
+        }
+        System.out.println();
+
+    }
+
+    public static void merge(int arr[] , int si , int mid , int ei){
+
+        int temp[] = new int [ei - si +1];
+
+        int i = si; // iterator for left part
+
+        int j = mid + 1; // iteratir for right part
+
+        int k = 0;  // iterator for temp arr
+
+
+        while( i <= mid && j <= ei){
+
+            if(arr[i] < arr[j]){
+                temp[k] = arr[i];
+                i++;
+                
+            }else{
+                temp[k] = arr[j];
+                j++;
+            }
+            k++;
+        }
+
+
+        // left part , for leftover elements.
+        while( i <= mid){
+            temp[k++] = arr[i++];
+        }
+
+        // right part 
+
+        while( j <= ei){
+            temp[k++] = arr[j++];
+        }
+
+
+        // copy temp to original arr
+
+        for( k = 0 ,i = si ; k < temp.length ; i++ , k++){
+
+            arr[i] = temp [k];
+        }
+
+
+    }
+    public static void merge_sort(int arr [] , int si , int ei){
+
+        if(si >= ei){
+            return;
+        }
+
+        // kaam 
+
+        int mid = si + (ei -si) / 2;
+
+        merge_sort(arr , si , mid); // left part
+
+        merge_sort(arr , mid+1 , ei); // right part
+
+        merge(arr , si , mid , ei);
+
+
+
+    }
+    public static void main(String args[]){
+
+        int arr [] = {6,3,9,5,2,8};
+
+        merge_sort(arr , 0 ,arr.length -1);
+
+        print_arr(arr);
+    }
+}
+
+
+
+
 
